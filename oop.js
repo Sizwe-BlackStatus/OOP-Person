a84947bd4160270453daae8e36b8889b162fa998
Sizwe <sizwemasemola1@gmail.com>
2020-04-20 09:21:51 +0200

    second commit

--- oop.js
+++ oop.js
@@ -6,10 +6,10 @@ class Person{
         this.interests = interests;
     }
     hello(){
-        let greeting = `Hello, my name is ${this.name} and I am a ${this.age} year old ${this.gender}. My interest are ${this.interests}.`
+        let greeting = `Hello, my name is ${this.name} and I am a ${this.age} year old ${this.gender}. My interest are ${this.interests[0]}, and ${this.interests[1]}.`
         return greeting;
     }
 }
 
-let person = new Person('Stracci', '55', 'Male', ['being a Mob-boss', ' run the streets', ' I love kids', ' I am one of the 5-families of New-York'])
+let person = new Person('Stracci', '55', 'Male', ['being a Mob-boss', ' I am one of the 5-families of New-York'])
 console.log(person.hello())
